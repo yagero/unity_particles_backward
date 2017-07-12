61a4f61c19cdc376d5e9f9c300d98f4a38e08bc8
yagero <yagero@gmail.com>
2017-07-12 13:54:56 +0200

    initial commit

---
+++ Assets/Scripts/ParticleSystemEx.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ParticleSystemEx
+{
+    public static Coroutine PlayBackward(this ParticleSystem self, MonoBehaviour mb, bool withChildren = true)
+    {
+        Debug.Assert(self);
+        return mb.StartCoroutine(CoPlayBackward(self, withChildren));
+    }
+
+    public static Coroutine PlayBackward(this ParticleSystem self, MonoBehaviour mb, float maxLifeTime, bool withChildren = true)
+    {
+        Debug.Assert(self);
+        return mb.StartCoroutine(CoPlayBackward(self, maxLifeTime, withChildren));
+    }
+
+    public static IEnumerator CoPlayBackward(this ParticleSystem self, bool withChildren = true)
+    {
+        Debug.Assert(self);
+        yield return self.CoPlayBackward(self.GetMaxLifeTime(withChildren), withChildren);
+    }
+
+    public static IEnumerator CoPlayBackward(this ParticleSystem self, float maxLifeTime, bool withChildren = true)
+    {
+        Debug.Assert(self);
+        Debug.Assert(maxLifeTime > 0f);
+        self.Stop(withChildren);
+        self.Clear(withChildren);
+        self.SetUseAutoRandomSeed(false, withChildren);
+        self.Play(withChildren);
+
+        float time = maxLifeTime;
+        while (time > 0f)
+        {
+            self.Simulate(time, true);
+            time -= Time.deltaTime;
+            yield return null;
+        }
+
+        self.Simulate(0f, true, true);
+    }
+
+    public static void SetUseAutoRandomSeed(this ParticleSystem self, bool value, bool withChildren = true)
+    {
+        Debug.Assert(self);
+        if (!withChildren) {
+            self.useAutoRandomSeed = value;
+        }
+        else {
+            var allps = self.GetComponentsInChildren<ParticleSystem>();
+            foreach (var ps in allps)
+                ps.useAutoRandomSeed = value;
+        }
+    }
+
+    public static float GetMaxLifeTime(this ParticleSystem self, bool withChildren = true)
+    {
+        Debug.Assert(self);
+        var maxLifeTime = float.MinValue;
+
+        if (!withChildren)
+        {
+            maxLifeTime = self.main.startLifetime.GetMaxValue();
+        }
+        else
+        {
+            var allps = self.GetComponentsInChildren<ParticleSystem>();
+            foreach (var ps in allps)
+                maxLifeTime = Mathf.Max(maxLifeTime, ps.main.startLifetime.GetMaxValue());
+        }
+
+        return maxLifeTime;
+    }
+
+    public static float GetMaxValue(this ParticleSystem.MinMaxCurve self)
+    {
+        switch (self.mode)
+        {
+            case ParticleSystemCurveMode.Constant:
+                return self.constant;
+            case ParticleSystemCurveMode.TwoConstants:
+                return self.constantMax;
+            case ParticleSystemCurveMode.Curve:
+            case ParticleSystemCurveMode.TwoCurves:
+            default:
+                return self.curveMax.GetMaxValue() * self.curveMultiplier;
+        }
+    }
+
+    public static float GetMaxValue(this AnimationCurve self)
+    {
+        float maxValue = float.MinValue;
+        for (int i = 0; i < self.length; ++i)
+            maxValue = Mathf.Max(maxValue, self.keys[i].value);
+        return maxValue;
+    }
+}
---
+++ Assets/Scripts/PlayBackwardTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(ParticleSystem))]
+public class PlayBackwardTest : MonoBehaviour
+{
+    Coroutine m_CoPlayBackward = null;
+
+    public void Play()
+    {
+        StopBackward();
+        var ps = GetComponentInChildren<ParticleSystem>();
+        ps.Stop();
+        ps.Clear();
+        ps.Play();
+    }
+
+    public void PlayBackward()
+    {
+        StopBackward();
+        var ps = GetComponentInChildren<ParticleSystem>();
+        m_CoPlayBackward = ps.PlayBackward(this); // = StartCoroutine(ps.CoPlayBackward());
+    }
+
+    void StopBackward()
+    {
+        if (m_CoPlayBackward != null)
+            StopCoroutine(m_CoPlayBackward);
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(PlayBackwardTest))]
+public class PlayBackwardTestEd : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var myScript = (PlayBackwardTest)target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Play"))
+        {
+            myScript.Play();
+        }
+
+        if (GUILayout.Button("Play Backward"))
+        {
+            myScript.PlayBackward();
+        }
+    }
+}
+#endif
+
+
